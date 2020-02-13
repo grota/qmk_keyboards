7fc3da4b5c41e3015368e8e0674059645cb39b19
Giuseppe Rota <rota.giuseppe@gmail.com>
2020-02-13 16:12:16 +0100

    reposition

--- dactyl_manuform/6x6/keymap.c
+++ dactyl_manuform/6x6/keymap.c
@@ -28,15 +28,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_6x6_sym(
      KC_ESC,     KC_C_1,  KC_C_2,        KC_C_3,      KC_C_4,  KC_C_5,                                KC_C_6,           KC_C_7,  KC_C_8,   KC_C_9, KC_C_0,   TT(_FN_AND_MOUSE),
-     KC_GRAVE,   KC_Q,    KC_MEDIA_DOWN, KC_MEDIA_UP, KC_HOME, TD_SQBRKTL,                            TD_SQBRKTR,       KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT, KC_BSLASH,
-     KC_MS_BTN3, KC_A,    KC_F,          KC_W,        KC_R,    KC_EQUAL,                              KC_QUOTE,         KC_H,    KC_J,     KC_K,   KC_L,     KC_END,
+     KC_GRAVE,   KC_Q,    KC_MEDIA_DOWN, KC_MEDIA_UP, KC_HOME, KC_END,                                KC_DELETE,        KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT, KC_BSLASH,
+     KC_MS_BTN3, KC_A,    KC_F,          KC_W,        KC_R,    KC_EQUAL,                              KC_QUOTE,         KC_H,    KC_J,     KC_K,   KC_L,     KC_ARROW,
      KC_LGUI,    KC_Z,    KC_S,          KC_D,        KC_G,    KC_X,                                  KC_Y,             KC_C,    KC_E,     KC_I,   KC_O,     KC_PGUP,
      KC_LCTRL,   KC_LALT, KC_T,          KC_M,        KC_V,    RALT_T(KC_SCOLON),                     RALT_T(KC_COMMA), KC_N,    KC_P,     KC_B,   KC_U,     KC_PGDOWN,
                           LCTL(KC_V),    TT(_FN_AND_MOUSE),                                                                      KC_SLASH, KC_DOT,
 
                                                              KC_BSPACE, LCTL_T(KC_TAB),                          RCTL_T(KC_ENTER), LALT_T(KC_SPACE),
                                                                            KC_LSPO, LCTL(KC_C),         KC_MINUS, KC_RSPC,
-                                                                           KC_PGDOWN,  KC_PGUP,         KC_ARROW, KC_DELETE
+                                                                           KC_PGDOWN,  KC_PGUP,         TD_SQBRKTL, TD_SQBRKTR
   ),
   [_FN_AND_MOUSE] = LAYOUT_6x6_sym(
        _______,_______,_______,_______,_______,  RESET,              RESET, _______,    _______,     KC_F11,     KC_F12,    TO(_BASE),
